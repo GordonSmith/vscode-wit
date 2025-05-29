8e4933c54837c7c4810e031a80223b5734be0a68
Victor Adossi <vadossi@cosmonic.com>
2025-05-29 23:02:40 +0900

    chore: add prettier
    This commit adds prettier config and integrates directly with eslint
    
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,41 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import markdown from "@eslint/markdown";
-import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
+import globals from 'globals';
+
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import markdown from '@eslint/markdown';
+import css from '@eslint/css';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier';
 
 export default defineConfig([
-    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+        plugins: { js, prettier },
+        extends: ['js/recommended'],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    },
     tseslint.configs.recommended,
     {
-        files: ["**/*.{ts,tsx,js,jsx}"],
+        files: ['**/*.{ts,tsx,js,jsx}'],
+        plugins: {
+            prettier,
+        },
         rules: {
             // Allow unused function parameters (disable rule)
             '@typescript-eslint/no-unused-vars': 'off',
-            'quotes': ['error', 'double']
-        }
+            quotes: ['error', 'double'],
+        },
     },
-    { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-    { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+    {
+        files: ['**/*.md'],
+        plugins: { markdown },
+        language: 'markdown/gfm',
+        extends: ['markdown/recommended'],
+    },
+    {
+        files: ['**/*.css'],
+        plugins: { css },
+        language: 'css/css',
+        extends: ['css/recommended'],
+    },
 ]);
